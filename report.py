5f75e817745b034914b7ccacbb3e54c1077f49b6
alex <zhurbitsky@gmail.com>
2017-08-11 12:01:25 +0300

    Refactoring

--- report.py
+++ report.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 #
 # Informative summary from logs.
 
@@ -7,42 +7,45 @@ import psycopg2
 DBNAME = "news"
 
 
-def report():
-    '''Main function which prints report.'''
-
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
-
-    # 1. What are the most popular three articles of all time?
-    c.execute("select title, count(*) as views from articles, log"
-              " where concat('/article/', articles.slug)=log.path"
-              " group by articles.title order by views desc limit 3")
-    top_news = c.fetchall()
+def print_top_articles():
+    """What are the most popular three articles of all time?"""
 
     print("1. The most popular three articles of all time:")
+
+    top_news = get_query_results(
+              "select title, count(*) as views from articles, log"
+              " where concat('/article/', articles.slug)=log.path"
+              " group by articles.title order by views desc limit 3")
+
     for news in top_news:
         print(' * "{}" - {} views'.format(news[0], news[1]))
 
-    print("")
 
-    # 2. Who are the most popular article authors of all time?
-    c.execute("select authors.name, sum(views) as views from"
+def print_top_authors():
+    """Who are the most popular article authors of all time?"""
+
+    print("2. The most popular article authors of all time:")
+
+    top_authors = get_query_results(
+              "select authors.name, sum(views) as views from"
               " (select author, count(*) as views from articles, log"
               " where concat('/article/',articles.slug)=log.path"
               " group by articles.slug, author) as article_views, authors"
               " where article_views.author=authors.id"
               " group by article_views.author, authors.name"
               " order by views desc")
-    top_authors = c.fetchall()
 
-    print("2. The most popular article authors of all time:")
     for author in top_authors:
         print(' * {} - {} views'.format(author[0], author[1]))
 
-    print("")
 
-    # 3. On which days more than 1% of requests lead to errors?
-    c.execute("select date, rate from"
+def print_top_error_days():
+    """On which days more than 1% of requests lead to errors?"""
+
+    print("3. Days which more than 1% of requests lead to errors:")
+
+    top_errors = get_query_results(
+              "select date, rate from"
               " (select to_char(time, 'Mon DD, YYYY') as date,"
               " round("
               "cast(100*sum(case when status!='200 OK' then 1 else 0 end)"
@@ -50,13 +53,38 @@ def report():
               "case when status='200 OK' then 1 else 0 end) as numeric), 1)"
               " as rate"
               " from log group by date) as err where rate>1")
-    top_errors = c.fetchall()
 
-    print("3. Days which more than 1% of requests lead to errors:")
     for day in top_errors:
         print(' * {} - {}%'.format(day[0], day[1]))
 
+
+def connect(database_name):
+    """Connect to the PostgreSQL database. Returns a database connection."""
+
+    try:
+        db = psycopg2.connect("dbname={}".format(database_name))
+        c = db.cursor()
+        return db, c
+    except psycopg2.Error as e:
+        print("Unable to connect to database")
+        sys.exit(1)
+
+
+def get_query_results(query):
+    """Executes query and returns result"""
+
+    db, c = connect(DBNAME)
+    c.execute(query)
+    results = c.fetchall()
+    db.commit()
     db.close()
 
+    return results
+
+
 if __name__ == '__main__':
-    report()
+    print_top_articles()
+    print("")
+    print_top_authors()
+    print("")
+    print_top_error_days()
